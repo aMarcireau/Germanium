818edc0a5fbb68d0e6704169cff9a3bada2bef86
Alexandre Marcireau <alexandre.marcireau@student.ecp.fr>
2015-03-14 18:24:26 +0100

    Adapt parser to use one list per title

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var germanium = require('./../');
 var path = require('path');
 
 germanium.subtitles(path.resolve(__dirname, './test'), ['srt', 'fcpxml'], [
-    [[0], 'Le premier sous-titre en français', [500]],
-    [[2, 0], 'Le second sous-titre'],
-    [[5, 300], 'Le troisième sous-titre', [7, 0]],
+    [0, 'Le premier sous-titre en français', 500],
+    [2, 0, 'Le second sous-titre'],
+    [5, 300, 'Le troisième sous-titre', 7, 0],
 ]);
